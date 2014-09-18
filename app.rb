46938e0d65f6edd922f43a7f336480947665e459
Brandon Hopkins <brhpkns@gmail.com>
2014-09-18 16:57:46 -0400

    debugging

--- app.rb
+++ app.rb
@@ -12,7 +12,10 @@ class ApplicationController < Sinatra::Base
     puts "User zip code: #{user.zip_code}"
     puts borough.zips.map{|x| x.code }
     zip_code = borough.zips.find_by(code: user.zip_code)
+    puts zip_code
+    puts Manhattantree.count
     if street = zip_code.streets.where(name: user.street).first
+      puts street
       tree_object = street.nearest_tree_to(user.building_num)
       tree_species = tree_object.species
       image = Image.find_by(species: tree_species)
