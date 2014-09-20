82887d8c6313d997c15380e7f03000505153754b
Brandon Hopkins <brhpkns@gmail.com>
2014-09-19 22:58:00 -0400

    Hopeful fix for problem of .min in nearest_tree_to returning nil

--- models/street.rb
+++ models/street.rb
@@ -17,12 +17,14 @@ class Street
   def nearest_tree_to(user_building_num)
     if self.zip.borough.name == "Brooklyn"
       trees_on_street_side = user_building_num.to_i.odd? ? self.trees.select{ |tree| tree.normalized_building_num.odd?} : self.trees.select{ |tree| tree.normalized_building_num.even?}
+      return nil if trees_on_street_side.empty?
       species = trees_on_street_side.map{ |tree| [(tree.normalized_building_num - user_building_num.to_i).abs, tree] }.min[1]
       if (species.building_num.to_i - user_building_num.to_i).abs > 10
         species = Tree.new(species: "UNKNOWN", building_num: user_building_num)
       end
     else 
       trees_on_street_side = user_building_num.to_i.odd? ? self.manhattantrees.select{ |tree| tree.normalized_building_num.odd?} : self.manhattantrees.select{ |tree| tree.normalized_building_num.even?}
+      return nil if trees_on_street_side.empty?
       species = trees_on_street_side.map{ |tree| [(tree.normalized_building_num - user_building_num.to_i).abs, tree] }.min[1]
     end
     return species
